baa08b07b954a74749d09a712d87e29a0d1b6d90
Alec John Zabasky <12651151+xCeezyx@users.noreply.github.com>
2023-12-23 18:02:25 -0800

    Add files via upload

---
+++ LethalEscape/LETimerFunctions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace LethalEscape
+{
+	internal class LETimerFunctions : MonoBehaviour
+	{
+		public static float MinuteEscapeTimerPuffer;
+		public static float MinuteEscapeTimerBracken;
+		public static float MinuteEscapeTimerHoardingBug;
+
+		public static LETimerFunctions instance;
+		private void Awake()
+		{
+			instance = this;
+		}
+		private void Update()
+		{
+			MinuteEscapeTimerBracken += Time.deltaTime;
+			MinuteEscapeTimerHoardingBug += Time.deltaTime;
+			MinuteEscapeTimerPuffer += Time.deltaTime;
+		}
+	}
+
+}
